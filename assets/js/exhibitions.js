ed4c445e57f1c8c61d74cf41aa27a9a3e1f94ac8
Alexander Garcia <agarcia@clarityinnovates.com>
2020-11-13 13:03:28 -0500

    updates 11

---
+++ assets/js/exhibitions.js
@@ -0,0 +1,49 @@
+
+function initialize () {
+  const apiKey = `1a1872a1-9c00-4650-8d3a-6dc6be211b16`;
+  const url = `https://api.harvardartmuseums.org/exhibition?apikey=${apiKey}&status=current`;
+
+  // Creates a Fetch request
+  fetch(url, {
+    method: 'GET',
+    type: "cors"
+  })
+  // Transforms response data into usable JSON format
+  .then(response => response.json())
+  // Takes the formatted JSON data and generates our results
+  .then(data => generateResults(data));
+}
+
+// Function that dynamically builds current exibitions
+function generateResults({ records }) {
+  // Loops through the records response array and creates an article to append to the #exhibitions element
+  records.forEach(({begindate = '-', enddate = '-', title, url, poster}) => {
+    // Creates an article with default className & innerHTML with our response data
+    var article = document.createElement('article');
+    article.className = "md:flex bg-gray-900 rounded-lg p-6 mr-4 mt-4 shadow max-w-3xl"
+    article.innerHTML = `<div class="flex-col"><h3 class="text-gray-400 text-left"><a href="${url}">${title}</a></h3><span class="block text-xs text-white">${begindate} thru ${enddate}</span></div>`
+    // Creates an img element with default classNames
+    var img = document.createElement('img');
+    img.className = "h-16 w-16 rounded-full mx-auto md:mx-0 md:mr-6";
+    
+    // If there isn't a poster object put in default, else from response data
+    if(!poster) {
+      img.alt = 'No poster'
+      img.src = './assets/images/empty.png';
+    } else {
+      img.alt = poster.caption;
+      img.src = poster.imageurl;
+    }
+    // Add the img element to the start of the article
+    article.prepend(img);
+    // Find the #exhibitions id and add the article
+    document.getElementById('exhibitions').appendChild(article);
+  })
+}
+
+// Add event listeners to button on load if not IE8
+if(window.addEventListener) {
+  window.addEventListener('load', initialize, false);
+} else {
+  window.attachEvent('onload', initialize, false);
+}
