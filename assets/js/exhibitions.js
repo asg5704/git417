61a59bdd5c415647d581c4caaf6edae90b02f462
Alexander Garcia <agarcia@clarityinnovates.com>
2020-11-24 09:55:11 -0500

    updates jquery

--- assets/js/exhibitions.js
+++ assets/js/exhibitions.js
@@ -18,32 +18,55 @@ function initialize () {
 function generateResults({ records }) {
   // Loops through the records response array and creates an article to append to the #exhibitions element
   records.forEach(({begindate = '-', enddate = '-', title, url, poster}) => {
-    // Creates an article with default className & innerHTML with our response data
-    var article = document.createElement('article');
-    article.className = "md:flex bg-gray-900 rounded-lg p-6 mr-4 mt-4 shadow max-w-3xl"
-    article.innerHTML = `<div class="flex-col"><h3 class="text-gray-400 text-left"><a href="${url}">${title}</a></h3><span class="block text-xs text-white">${begindate} thru ${enddate}</span></div>`
     // Creates an img element with default classNames
-    var img = document.createElement('img');
-    img.className = "h-16 w-16 rounded-full mx-auto md:mx-0 md:mr-6";
-    
+    // var img = document.createElement('img');
+    // img.className = "h-16 w-16 rounded-full mx-auto md:mx-0 md:mr-6";
+
     // If there isn't a poster object put in default, else from response data
-    if(!poster) {
-      img.alt = 'No poster'
-      img.src = './assets/images/empty.png';
-    } else {
-      img.alt = poster.caption;
-      img.src = poster.imageurl;
-    }
+    // if(!poster) {
+    //   img.alt = 'No poster'
+    //   img.src = './assets/images/empty.png';
+    // } else {
+    //   img.alt = poster.caption;
+    //   img.src = poster.imageurl;
+    // }
+
+    // jQuery version
+    var img = $('<img />')
+      .addClass('h-16 w-16 rounded-full mx-auto md:mx-0 md:mr-6')
+      .attr('src', !poster ? './assets/images/empty.png' : poster.imageurl)
+      .attr('alt', !poster ? 'No poster' : poster.caption)
+
+    // Creates an article with default className & innerHTML with our response data
+    // var article = document.createElement('article');
+    // article.className = "md:flex bg-gray-900 rounded-lg p-6 mr-4 mt-4 shadow max-w-3xl"
+    // article.innerHTML = `<div class="flex-col"><h3 class="text-gray-400 text-left"><a href="${url}">${title}</a></h3><span class="block text-xs text-white">${begindate} thru ${enddate}</span></div>`
+
+    // jQuery version
+    var article = $('<article></article>')
+      .addClass('md:flex bg-gray-900 rounded-lg p-6 mr-4 mt-4 shadow max-w-3xl')
+      .html(`<div class="flex-col"><h3 class="text-gray-400 text-left"><a href="${url}">${title}</a></h3><span class="block text-xs text-white">${begindate} thru ${enddate}</span></div>`)
+      .prepend(img)
+
+    
+    
+    
     // Add the img element to the start of the article
-    article.prepend(img);
+    // article.prepend(img);
+
     // Find the #exhibitions id and add the article
-    document.getElementById('exhibitions').appendChild(article);
+    // document.getElementById('exhibitions').appendChild(article);
+    // jQuery version
+    $('#exhibitions').append(article)
   })
 }
 
 // Add event listeners window object on load if not IE8
-if(window.addEventListener) {
-  window.addEventListener('load', initialize, false);
-} else {
-  window.attachEvent('onload', initialize, false);
-}
+// if(window.addEventListener) {
+//   window.addEventListener('load', initialize, false);
+// } else {
+//   window.attachEvent('onload', initialize, false);
+// }
+
+// jQuery version
+$(document).ready(initialize);
